b76b6f9909ca059c36904afa71a83abf5bf9a23a
Finley Williamson V <finley.williamson@gmail.com>
2022-08-30 15:38:37 -0400

    move random image card inside random image page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PageTitleComponent } from './components/page-title/page-title.component';
-import { RandomImageCardComponent } from './components/random-image-card/random-image-card.component';
+import { RandomImageCardComponent } from './components/random-image-page/random-image-card/random-image-card.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
